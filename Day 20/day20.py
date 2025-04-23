8d1819bfa0893fc14d8a5eb70d15ab953d90381e
Dave Rayment <dave.rayment@gmail.com>
2025-04-23 21:43:58 +0100

    Faster Day 20

--- Day 20/day20.py
+++ Day 20/day20.py
@@ -1,18 +1,26 @@
 DIRECTIONS = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
-def do_search(track, time_limit = 2, min_saving = 100):
-    shortcuts = []
-    track_len = len(track)
-    
-    for i, (x, y) in enumerate(track):
-        for j in range(i + min_saving, track_len):
-            x1, y1 = track[j]
-            shortcut_time = abs(x - x1) + abs(y - y1)
-            saving = j - i - shortcut_time
-            if shortcut_time <= time_limit and saving >= min_saving:
-                shortcuts.append(((x, y), (x1, y1), saving))
+# Gets all the coordinate offsets and times which can be reached at or under the limit.
+def get_offsets(limit):
+    return set([
+        (x, y, abs(x) + abs(y))
+        for x in range(-limit, limit + 1)
+        for y in range(-limit, limit + 1)
+        if abs(x) + abs(y) <= limit
+    ])
 
-    return shortcuts
+def do_search(track, track_positions, time_limit = 2, min_saving = 100):
+    shortcut_count = 0
+    offsets = get_offsets(time_limit)
+
+    for i, (x, y) in enumerate(track):
+        for dx, dy, time in offsets:
+            x1, y1 = x + dx, y + dy
+            track_pos = track_positions.get((x1, y1), None)
+            if track_pos is not None and track_pos - i - time >= min_saving:
+                shortcut_count += 1
+
+    return shortcut_count
 
 
 with open('Day 20/input.txt', 'r') as f:
@@ -26,16 +34,20 @@ for y, line in enumerate(lines):
 # Map out the track, including start and end.
 x, y = start
 track = [start]
+track_positions = {start: 0}    # fast lookup for track position based on coordinate
+track_len = 1
 while lines[y][x] != 'E':
     for dx, dy in DIRECTIONS:
         x1, y1 = x + dx, y + dy
         if lines[y1][x1] != '#' and (x1, y1) not in track:
             track.append((x1, y1))
+            track_positions[(x1, y1)] = track_len
             x, y = x1, y1
+            track_len += 1
             break
 
-shortcuts = do_search(track, time_limit = 2, min_saving = 100)
-print(f'Day 20 Part 1: {len(shortcuts)}')
+shortcuts = do_search(track, track_positions, time_limit = 2)
+print(f'Day 20 Part 1: {shortcuts}')
 
-shortcuts = do_search(track, time_limit = 20)
-print(f'Day 20 Part 2: {len(shortcuts)}')
+shortcuts = do_search(track, track_positions, time_limit = 20)
+print(f'Day 20 Part 2: {shortcuts}')
